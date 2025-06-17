5929ffd68d9803ae823ff1c26179f9adf9f38566
Ujwal004 <245122749004@mvsrec.edu.in>
2025-06-17 15:53:26 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        double average = (double) sum / 2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
